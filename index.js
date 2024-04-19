9b30516f6d628d357f9baf68165c222d2816b639
nisol <nisolmedinaue@gmail.com>
2024-04-19 11:49:37 +0200

    last

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.5 });
+  
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach(el => observer.observe(el));
+  });
+  
+  
+  
   
